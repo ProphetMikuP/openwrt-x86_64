2f5bf27cc7c465d369e38efba8c73952b3c7cace
ProphetMikuP <62280559+ProphetMikuP@users.noreply.github.com>
2022-03-01 23:30:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,26 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+
+git https://github.com/AlexZhuo/luci-app-bandwidthd.git
+git https://github.com/destan19/OpenAppFilter.git
+git https://github.com/esirplayground/luci-app-poweroff.git
+git https://github.com/frainzy1477/luci-app-clash.git
+git https://github.com/fw876/helloworld.git
+git https://github.com/garypang13/luci-theme-edge.git
+git https://github.com/honwen/luci-app-aliddns.git
+git https://github.com/jerrykuku/openwrt-package.git
+git https://github.com/kenzok8/openwrt-packages.git
+git https://github.com/lisaac/luci-app-dockerman.git
+git https://github.com/openwrt-develop/luci-theme-atmaterial.git
+git https://github.com/project-lede/luci-app-godproxy.git
+git https://github.com/rufengsuixing/luci-app-adguardhome.git
+git https://github.com/sirpdboy/sirpdboy-package.git
+git https://github.com/tty228/luci-app-serverchan.git
+git https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
+git https://github.com/xiaorouji/openwrt-passwall.git
+
+./scripts/feeds update -a
+./scripts/feeds install -a
